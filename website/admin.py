9f28ef19cc95da37ca62227c9c488f03876539a3
Isaac Parker <parrotmac@gmail.com>
2017-05-25 01:22:22 -0600

    Aaaaaand still forgot that other ting

--- website/admin.py
+++ website/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 import website.models
 
 admin.site.register(website.models.Event)
+admin.site.register(website.models.EventCategory)
 admin.site.register(website.models.MenuEntry)
