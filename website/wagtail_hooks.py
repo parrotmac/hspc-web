f23eadc387d8b09b60637a9c18ee3936a510400f
Isaac Parker <parrotmac@gmail.com>
2017-08-08 00:50:37 -0600

    Fixing API, footer, benefactors

--- website/wagtail_hooks.py
+++ website/wagtail_hooks.py
@@ -7,7 +7,7 @@ class EventModelAdmin(ModelAdmin):
     model = Event
     # menu_label = 'Events'  # ditch this to use verbose_name_plural from model
     menu_icon = 'date'  # change as required
-    menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
+    menu_order = 230  # will put in 3rd place (000 being 1st, 100 2nd)
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = False # or True to exclude pages of this type from Wagtail's explorer view
     list_display = (
@@ -25,7 +25,7 @@ class EventModelAdmin(ModelAdmin):
 class EventCategoryAdmin(ModelAdmin):
     model = EventCategory
     menu_icon = 'date'  # change as required
-    menu_order = 300  # will put in 3rd place (000 being 1st, 100 2nd)
+    menu_order = 220  # will put in 3rd place (000 being 1st, 100 2nd)
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = False # or True to exclude pages of this type from Wagtail's explorer view
     list_display = (
