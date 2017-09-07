5c327b8b87e98515bf48e5f3bebfebed55d20c3f
Isaac Parker <parrotmac@gmail.com>
2017-09-07 12:50:34 -0600

    Changing logos around for fun.

---
+++ website/static/scripts/nav.js
@@ -0,0 +1,18 @@
+//
+// const navigationRoot = document.querySelector("#hoverDropdownNavbar");
+//
+// const toggleLinks = navigationRoot.querySelectorAll(".dropdown-toggle");
+//
+// function toggleClass(element, className) {
+//     if(element.classList.contains(className)) {
+//         element.classList.remove(className)
+//     } else {
+//         element.classList.add(className)
+//     }
+// }
+//
+// for (let i = 0; i < toggleLinks.length; i++) {
+//     toggleLinks[i].addEventListener("click", event => {
+//         toggleClass(event.target, "open");
+//     }, false)
+// }
