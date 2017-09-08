80fad76dc9b0f752b3338671a012d2cc5f3e8283
Isaac Parker <parrotmac@gmail.com>
2017-09-07 18:42:25 -0600

    Rewrote the navbar, added mobile toggle

--- website/static/scripts/nav.js
+++ website/static/scripts/nav.js
@@ -1,18 +1,35 @@
+/* See https://codepen.io/parrotmac/pen/eEajYz for demo */
+// const dropDownListLinks = document.querySelectorAll("li.dropdown>a");
 //
-// const navigationRoot = document.querySelector("#hoverDropdownNavbar");
-//
-// const toggleLinks = navigationRoot.querySelectorAll(".dropdown-toggle");
-//
-// function toggleClass(element, className) {
-//     if(element.classList.contains(className)) {
-//         element.classList.remove(className)
+// for(let i = 0; i < dropDownListLinks.length; i++) {
+//   dropDownListLinks[i].addEventListener("click", event => {
+//     const _li = event.target.parentElement;
+//     if(_li.classList.contains("open")) {
+//       _li.classList.remove("open")
 //     } else {
-//         element.classList.add(className)
+//       const _allDropdowns = _li.parentElement.querySelectorAll(".dropdown");
+//       for(let j = 0; j < _allDropdowns.length; j++) {
+//         _allDropdowns[j].classList.remove("open")
+//       }
+//       _li.classList.add("open")
 //     }
+//   }, false)
 // }
-//
-// for (let i = 0; i < toggleLinks.length; i++) {
-//     toggleLinks[i].addEventListener("click", event => {
-//         toggleClass(event.target, "open");
-//     }, false)
-// }
+
+var mainMenu = document.querySelector(".hoverDropdownNavbar");
+function setMenuOpen (showMenu) {
+    showMenu ? mainMenu.classList.add("mobile-open") : mainMenu.classList.remove("mobile-open");
+}
+
+document.getElementById("nav-toggle").addEventListener("click", function(event) {
+    var toggleBtn = event.target;
+    if(toggleBtn.classList.contains("open")) {
+        // Close up
+        toggleBtn.classList.remove("open");
+        setMenuOpen(false);
+    } else {
+
+        toggleBtn.classList.add("open");
+        setMenuOpen(true);
+    }
+}, false);
