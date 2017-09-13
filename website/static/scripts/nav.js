4c6ee4830a5f892d486efe9382f2080f71e5c5f4
Isaac Parker <parrotmac@gmail.com>
2017-09-12 18:55:27 -0600

    Using a simple hover design for navbar. Not mobile compatible yet

--- website/static/scripts/nav.js
+++ website/static/scripts/nav.js
@@ -1,27 +1,9 @@
-/* See https://codepen.io/parrotmac/pen/eEajYz for demo */
-// const dropDownListLinks = document.querySelectorAll("li.dropdown>a");
-//
-// for(let i = 0; i < dropDownListLinks.length; i++) {
-//   dropDownListLinks[i].addEventListener("click", event => {
-//     const _li = event.target.parentElement;
-//     if(_li.classList.contains("open")) {
-//       _li.classList.remove("open")
-//     } else {
-//       const _allDropdowns = _li.parentElement.querySelectorAll(".dropdown");
-//       for(let j = 0; j < _allDropdowns.length; j++) {
-//         _allDropdowns[j].classList.remove("open")
-//       }
-//       _li.classList.add("open")
-//     }
-//   }, false)
-// }
-
-var mainMenu = document.querySelector(".hoverDropdownNavbar");
+var mainMenu = document.querySelector(".ddNavbar");
 function setMenuOpen (showMenu) {
     showMenu ? mainMenu.classList.add("mobile-open") : mainMenu.classList.remove("mobile-open");
 }
 
-var navToggle = document.getElementById("nav-toggle");
+var navToggle = document.querySelector(".nav-toggle");
 navToggle.addEventListener("click", function() {
     if(navToggle.classList.contains("open")) {
         // Close up
